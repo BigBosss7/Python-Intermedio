888145f1fdf5d5614d5eb4112e5d8494117a98fb
Big Boss <saul.b07@gmail.com>
2025-10-21 08:31:15 -0600

    agregando notas sobre pandas

--- leer_csv.py
+++ leer_csv.py
@@ -1,6 +1,6 @@
 import csv  
 
-with open("datos.csv") as archivo_csv:  
+with open("datos.csv", encoding="utf-8", newline='') as archivo_csv:  
 
     reader = csv.reader(archivo_csv)
     for row in reader:
--- leer_csv_con_pandas.py
+++ leer_csv_con_pandas.py
@@ -1,2 +1,32 @@
 import pandas as pd 
-print(type(pd))
+
+#usando la funcion read_csv de pandas para leer un archivo csv
+df = pd.read_csv("datos.csv",  encoding="utf-8")
+df2 = pd.read_csv("datos.csv",  encoding="utf-8")
+
+#obteniendo los datos de la columna 'nombre' y 'edad'
+#nombres = df["nombre"]
+
+#ordenando el dataframe por la columna 'edad'
+#df_ordenado = df.sort_values("edad")
+
+#ordenado de forma descendente
+df_ordenado_desc = df.sort_values("edad", ascending=False)
+
+#concatenndo los dos dataframes
+df_concatenado = pd.concat([df, df2])
+
+#accediendo a la  primer fila  conm head()
+primera_fila = df.head(1)   
+
+#accerdiendo a las ultimas 2 filas con tail()
+ultimas_filas = df.tail(2)
+
+#accediendo a la cantidad de filas y columnas con shape
+filas_totales, columnas_totales = df.shape
+
+#obteniendo estadisticas descriptivas del dataframe con describe()
+estadisticas = df.describe()
+
+#accediendo a un elemento especifico con loc (por etiqueta)
+elemento_loc = df.loc[0, "nombre"]
