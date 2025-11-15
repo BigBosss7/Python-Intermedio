46f898241686f5bbbe9f30411b7fb81a54dd8f4f
Big Boss <saul.b07@gmail.com>
2025-11-14 21:00:30 -0600

    feat: get, post, put en apis

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -3,8 +3,78 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+#Entidad User 
+class User(BaseModel):
+    id: int
+    name: str 
+    surname: str
+    url: str
+    age: int
+    
+users_list = [User(id= 1, name="Brais", surname="Moure", url="https://moure.dev", age=35),
+        User(id= 2, name ="Saul", surname="Bigboss", url="https://moure.com", age=38),
+        User(id= 3, name="Chanchito", surname="Feliz", url="https://chanchito.com", age=33)
+        ]   
+    
+    
+@app.get("/usersjson")
+async def usersjson():
+    return [{"name": "Brais", "surname":"Moure", "url":"https://moure.dev", "age": 35},
+            {"name": "Saul", "surname":"Bigboss", "url":"https://moure.com", "age": 38},
+            {"name": "Chanchito", "surname":"Feliz", "url":"https://chanchito.com", "age": 33}]
+    
+
 @app.get("/users")
 async def users():
-    return [{"name": "Brais", "surname":"Moure", "url":"https://moure.dev"},
-            {"name": "Saul", "surname":"Bigboss", "url":"https://moure.com"},
-            {"name": "Cahnchito", "surname":"Feliz", "url":"https://chanchito.com"}]
+    return users_list
+ 
+# Path
+@app.get("/user/{id}")
+async def user(id: int):
+    return search_user(id)
+    
+# Query  
+@app.get("/user/")
+async def user(id: int):
+    return search_user (id)
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:    
+        users_list.append(user)
+        
+@app.put("/user/")
+async def user(user: User):
+    
+    found = False 
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+
+    if not found:
+        return {"error": "No se ha actualizado el usuario"}
+    
+    
+@app.delete("/user/{id}")
+async def user(id: int):
+    
+    found = False
+    
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+            
+    if not found:
+            return {"error": "No se ha eliminado el usuario"}
+
+def search_user (id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "No se ha encontrado el usuario"}
+    
